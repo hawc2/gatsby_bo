8c381cc187b1fc6daac4366cd01188cf55b676d4
Alex Wermer-Colan <alwermercolan@gmail.com>
2022-04-23 21:10:16 -0400

    updating starter files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,27 @@
-module.exports = themeOptions => {
-  return {
-    plugins: [
-      {
-        resolve: `gatsby-transformer-ceteicean`,
-        options: {
-          applyBefore: themeOptions.applyBefore || [],
-          applyAfter: themeOptions.applyAfter || [],
-          namespaces: themeOptions.namespaces
-        }
+const basePath = '/gatsby-ceteicean-starter'
+
+module.exports = {
+  pathPrefix: basePath,
+  siteMetadata: {
+    title: `Group Project`,
+    description: `A template for a group project.`,
+    author: `Tutors`
+  },
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    `gatsby-theme-ceteicean`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/content/tei`,
       },
-    ]
-  }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/content/pages`,
+        name: `html`,
+      },
+    },
+  ],
 }
